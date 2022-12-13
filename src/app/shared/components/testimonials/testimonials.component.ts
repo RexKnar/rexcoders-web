2758bcb1d233a2a5fda8fe66a005d8c928fca18c
AbarnaRM <abarna.rm@rexknar.com>
2022-12-13 14:02:03 +0530

    testimonial-component-update-1

--- src/app/shared/components/testimonials/testimonials.component.ts
+++ src/app/shared/components/testimonials/testimonials.component.ts
@@ -1,39 +1,81 @@
 import { Component, OnInit } from '@angular/core';
+// import { Testimonials } from '../../models/testimonials.model';
 
 @Component({
   selector: 'app-testimonials',
   templateUrl: './testimonials.component.html',
-  styleUrls: ['./testimonials.component.scss']
+  styleUrls: ['./testimonials.component.scss'],
 })
 export class TestimonialsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
- 
-
-  slideConfig = {"slidesToShow": 3, "slidesToScroll": 3,
-  responsive: [
+  testimonialsList: any = [
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3
-      }
+      personImage: 'assets/img/user-1.jpg',
+      companyLogo: 'assets/img/c-1.png',
+      personName: 'Susan D. Murphy',
+      designation: 'CEO, Leader',
+      description: 'Faucibus tristique felis potenti ultrices ornare rhoncus',
+      rating: 4.9,
     },
     {
-      breakpoint: 800,
-      settings: {
-        slidesToShow: 2
-      }
+      companyLogo: 'assets/img/c-2.png',
+      personImage: 'assets/img/user-2.jpg',
+      personName: 'Maxine E. Gagliardi',
+      designation: 'Apple CEO',
+      description: 'Faucibus tristique felis potenti ultrices ornare rhoncus',
+      rating: 4.9,
     },
     {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1
-      }
-    }
-  ]};
-  
+      companyLogo: 'assets/img/c-3.png',
+      personImage: 'assets/img/user-3.jpg',
+      personName: 'Roy M. Cardona',
+      designation: 'Google Founder',
+      description: 'Faucibus tristique felis potenti ultrices ornare rhoncus',
+      rating: 4.9,
+    },
+    {
+      companyLogo: 'assets/img/c-4.png',
+      personImage: 'assets/img/user-4.jpg',
+      personName: 'Dorothy K. Shipton',
+      designation: 'Linkedin Leader',
+      description: 'Faucibus tristique felis potenti ultrices ornare rhoncus',
+      rating: 4.9,
+    },
+    {
+      companyLogo: 'assets/img/c-5.png',
+      personImage: 'assets/img/user-5.jpg',
+      personName: 'Robert P. McKissack',
+      designation: 'CEO, Leader',
+      description: 'Faucibus tristique felis potenti ultrices ornare rhoncus',
+      rating: 4.9,
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  slideConfig = {
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 }
---
+++ src/app/shared/models/testimonials.model.ts
@@ -0,0 +1,8 @@
+export class Testimonials{
+    personImage: any;
+    companyLogo: any;
+    personName : string;
+    designation : string;
+    description : string;
+    rating : number;
+}
